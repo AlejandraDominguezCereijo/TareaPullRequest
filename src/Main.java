492c2ffeebc0cea6b0e4edaacc99c621047c27b6
Sara <113593925+sram-daw@users.noreply.github.com>
2023-04-17 17:40:47 +0200

    close #3 nuevo sout (#4)
    Co-authored-by: sara.ramilo <sara.ramilo@mad.vass.es>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola comunidad de vecinos");
+        System.out.println("Holita vecinitos");
     }
 }
